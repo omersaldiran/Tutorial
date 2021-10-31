d553ff6cfc3aa035f540e64c4962d688cfeb6151
osaldiran <osaldiran@gmail.com>
2021-10-31 21:55:45 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string merhaba = "Merhaba";
+            bool dogrumu = false;
+            if (dogrumu == true)
+            {
+                Console.WriteLine("Evet doğru!");
+            }
+            else
+            {
+                Console.WriteLine("Hayır yanlış!");
+            }
+            Console.WriteLine(merhaba);
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+
+            urun1.Adi = "Elma";
+            urun1.Aciklama = "Amasya Elması";
+            urun1.Fiyati = 15;
+
+            Urun urun2 = new Urun();
+
+            urun2.Adi = "Karpuz";
+            urun2.Aciklama = "Diyarbakır";
+            urun2.Fiyati = 30;
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------");
+            }
+
+            Console.WriteLine("------------------Metotlar------------------");
+
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil", 12);
+            sepetManager.Ekle2("Elma", "Yeşil", 9);
+
+        }
+    }
+}
