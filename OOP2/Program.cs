8424931af7453bcc826c5c3f40ecb356e036ea3a
osaldiran <osaldiran@gmail.com>
2021-11-07 17:47:42 +0300

    Polymofizm ve Interface

---
+++ OOP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ömer Saldıran
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Ömer";
+            musteri1.Soyadi = "Saldıran";
+            musteri1.TcNo = "1234568910";
+
+            // Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 1;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "12345667899";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri4);
+
+
+        }
+    }
+}
